516218c7dd2ec1e49c9f828dc8578d8b5376a437
rand00 <oth.rand@gmail.com>
2015-03-27 13:35:22 +0100

    shell-script for running super-collider synths

---
+++ run_scsynth.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+#/bin/sh
+
+## This script is based off of the following guide;
+## http://swiki.hfbk-hamburg.de/MusicTechnology/634 
+
+SCSYNTH=$(which scsynth)
+PORT=57110
+export SC_SYNTHDEF_PATH="./test_synths" 
+#< this get's loaded, but is not set as standard when writing synths
+
+#export SC_JACK_INPUTS=2
+#export SC_JACK_OUTPUTS=2
+
+if [[ $SC_JACK_DEFAULT_INPUTS == "" ]]
+then 
+    #export SC_JACK_DEFAULT_INPUTS="alsa_pcm:capture_1,alsa_pcm:capture_2"
+    export SC_JACK_DEFAULT_INPUTS="system"
+fi
+
+if [[ $SC_JACK_DEFAULT_OUTPUTS == "" ]]
+then 
+    #export SC_JACK_DEFAULT_OUTPUTS="alsa_pcm:playback_1,alsa_pcm:playback_2"
+    export SC_JACK_DEFAULT_OUTPUTS="system"
+fi
+
+# like this it's not necessary to run this script as root < (because of being in build-dir?)
+# and it shows you how the server is actually started
+SCCMD="$SCSYNTH -i ${SC_JACK_INPUTS} -o ${SC_JACK_OUTPUTS} -u $PORT $@"
+echo $SCCMD "$@"
+
+$SCCMD 2>&1
+
+#nice -n -10 $SCCMD || $SCCMD
+
