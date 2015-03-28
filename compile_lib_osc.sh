1c19bc226ea456243c171b9d471b5759b069292b
rand00 <oth.rand@gmail.com>
2015-03-28 14:13:27 +0100

    Added/modified compile-scripts for all libs and test

--- compile_lib_osc.sh
+++ compile_lib_osc.sh
@@ -6,6 +6,6 @@ cd lib_osc
 ocamlbuild -clean
 ocaml setup.ml -configure --enable-tests --enable-lwt
 ocaml setup.ml -build
-ocaml setup.ml -install
+#ocaml setup.ml -install
 cd ..
 
---
+++ compile_sC_byte.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+#important to use -Is instead of -I
+ocamlbuild -use-ocamlfind -cflag -bin-annot \
+    -Is src,test \
+    sC.byte
